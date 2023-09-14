0f13a6872bdd6c054ff5dcbf6993bebb32c27cca
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-14 10:38:14 +0700

    feat: create user API

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 const app = express();
 const dbConfig = require('./dbConnect');
 const carRoute = require('./routes/carRoute')
+const userRoute = require('./routes/userRoute')
+
+app.use(express.json());
 app.use('/api/car', carRoute);
+app.use('/api/users', userRoute);
 const port = process.env.PORT || 5000;
-app.listen(port, ()=> console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
