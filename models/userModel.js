9b7f0e9bf2a9b846c6bf6a41f3f04c99422f00c6
PetrusAriaa <petrusariacr25@gmail.com>
2023-09-14 10:38:01 +0700

    feat: create user schema

---
+++ models/userModel.js
@@ -0,0 +1,13 @@
+// import Schema and model from mongoose
+const { Schema, models, model } = require("mongoose");
+
+const userSchema = Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    telp: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now(), required: true },
+});
+
+const userModel = models.User || model('user', userSchema);
+module.exports = userModel;
