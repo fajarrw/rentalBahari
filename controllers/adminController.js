6b50c0b665785a65480edacf8609fae2f0977728
hashifaq <hashifaq@gmail.com>
2023-09-26 17:49:21 +0700

    add api admin

---
+++ controllers/adminController.js
@@ -0,0 +1,89 @@
+const Admin = require('../models/adminModel')
+
+const getAllAdmin = async (req, res) => {
+    try {
+        const admin = await Admin.find({})
+        res.status(200).json({ admin: admin })
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ message: err });
+    }
+}
+
+const getAdminById = async (req, res) => {
+    try {
+        const _id = req.params.id
+        const admin = await Admin.findById(_id)
+
+        // handle null admin
+        if (!admin) {
+            res.status(404).json({ error: 'Admin not found' })
+            return
+        }
+
+        res.status(200).json({ admin })
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+}
+
+const deleteAdmin = async (req, res) => {
+    try {
+        const _id = req.params.id
+        const adminToDelete = await Admin.findById(_id)
+        if (!adminToDelete) {
+            res.status(404).json({ message: 'Admin not exists' })
+            return
+        }
+        await Admin.deleteOne({ _id })
+        res.status(204).json({ message: 'Admin updated successfully' })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: err })
+    }
+}
+
+const createAdmin = async (req, res) => {
+	try {
+		const { username, password } = await req.body
+
+		//input validation
+		if (!username || !password) {
+			res.status(400).json({ error: "Bad request. Missing required fields" })
+			return
+		}
+
+		const adminData = {
+			username,
+			password,
+		}
+		const newAdmin = await Admin.create(adminData)
+		const savedAdminData = await newCar.save()
+		res.status(201).json({ message: "Admin created successfully", admin: savedAdmminData })
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ error: err })
+	}
+};
+
+const editAdmin = async (req, res) => {
+    try {
+        const _id = req.params.id
+        const { username, password } = req.body
+        const adminToEdit = await Admin.findById(_id)
+        if (!adminToEdit) {
+            res.status(404).json({ message: 'Admin does not exist' })
+            return
+        }
+        await Admin.updateOne({ _id: _id }, {
+            username: username,
+            password: password,
+        })
+        res.status(204).json({ message: 'Admin updated successfully' })
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: err })
+    }
+}
+
+module.exports = { getAllAdmin, getAdminById, createAdmin, deleteAdmin, editAdmin }
--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const dbConfig = require('./dbConnect');
 const carRoute = require('./routes/carRoute')
 const userRoute = require('./routes/userRoute')
 const assuranceRoute = require('./routes/assuranceRoute')
+const adminRoute = require('./routes/adminRoute')
 
 app.use(express.json());
 app.use('/api/car', carRoute);
 app.use('/api/users', userRoute);
 app.use('/api/assurance', assuranceRoute)
+app.use('/api/admin', adminRoute)
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
