9e10ed8d1e7c9a7173efd32b384d20abccd60f60
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-09-27 11:17:26 +0700

    feat: create and delete assurance id field in user
    Here are the new features:
    1. Create assurance id field in user when creating a new assurance
    2. Delete assurance id field in user when deleting an assurance

--- controllers/assuranceController.js
+++ controllers/assuranceController.js
@@ -1,4 +1,5 @@
 const Assurance = require('../models/assuranceModel');
+const User = require('../models/userModel');
 const Validator = require('jsonschema').Validator;
 
 //getall
@@ -14,46 +15,57 @@ const getAllAssurance = async (req, res) => {
 
 //get one
 const getAssuranceById  = async (req, res) => {
-  try {
-      const _id = req.params.id
-      const assurance = await Assurance.findById(_id)
+	try {
+		const _id = req.params.id
+		const assurance = await Assurance.findById(_id)
 
-      // handle null user
-      if (!assurance) {
-          res.status(404).json({ error: 'Assurance not found' })
-          return
-      }
+		// handle null user
+		if (!assurance) {
+			res.status(404).json({ error: 'Assurance not found' })
+			return
+		}
 
-      res.status(200).json({ assurance: assurance })
-  } catch (err) {
-      res.status(500).json({ error: err })
-  }
+		res.status(200).json({ assurance: assurance })
+	} catch (err) {
+		res.status(500).json({ error: err })
+	}
 };
 
 //create
 const createAssurance = async (req, res) => {
     try {
-      const { alamat, nik, foto_ktp } = req.body;
-  
-      // Input validation
-      if (!alamat || !nik || !foto_ktp) {
-        return res.status(400).json({ error: "Bad request. Missing required fields" });
-      }
-  
-      // Create a new assurance document
-      const newAssurance = new Assurance({
-        alamat,
-        nik,
-        foto_ktp,
-      });
-  
-      // Save the new assurance document to the database
-      const savedAssurance = await newAssurance.save();
-  
-      res.status(201).json({ message: "Assurance created successfully", assurance: savedAssurance });
+		const { alamat, nik, foto_ktp, userID } = req.body;
+
+		// Input validation
+		if (!alamat || !nik || !foto_ktp || !userID) {
+			return res.status(400).json({ error: "Bad request. Missing required fields" });
+		}
+	
+		// Check the existence of user
+		const user = await User.findById(userID);
+		if (!user) {
+			res.status(400).json({ error: "Bad request. User does not exist" });
+			return;
+		}
+
+		// Create a new assurance document
+		const newAssurance = new Assurance({
+			alamat,
+			nik,
+			foto_ktp,
+		});
+	
+		// Save the new assurance document to the database
+		const savedAssurance = await newAssurance.save();
+	
+		// Save assurance id to its user
+		user.assuranceId = savedAssurance._id;
+		await user.save();
+
+		res.status(201).json({ message: "Assurance created successfully", assurance: savedAssurance });
     } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: err });
+		console.error(err);
+		res.status(500).json({ error: err });
     }
   };
 
@@ -68,6 +80,12 @@ const deleteAssurance = async (req, res) => {
         if (!existingAssurance) {
         return res.status(404).json({ error: "Assurance not found" });
         }
+
+		// Find the user that has the assurance id
+		await User.updateOne(
+			{ assuranceId: assuranceId },
+			{ $unset: { assuranceId: "" }}
+		);
 
         // Delete the assurance document
         await Assurance.findByIdAndDelete(assuranceId);
@@ -90,20 +108,20 @@ const editAssurance = async (req, res) => {
 
     //validate alamat json
     const schema = {
-      type: 'object',
-      properties: {
+		type: 'object',
+		properties: {
         jalan: { type: 'string' },
         kelurahan: { type: 'string' },
         kecamatan: { type: 'string' },
         kota: { type: 'string' },
         provinsi: { type: 'string' },
-      },
-      required: ['jalan', 'kelurahan', 'kecamatan', 'kota', 'provinsi'],
+		},
+		required: ['jalan', 'kelurahan', 'kecamatan', 'kota', 'provinsi'],
     };
     const validator = new Validator();
     if (!validator.validate(req.body.alamat, schema).valid){
-      res.status(400).json({ message: "Missing alamat attribute or unmatched schema" })
-      return;
+		res.status(400).json({ message: "Missing alamat attribute or unmatched schema" })
+		return;
     }
 
 		const _id = req.params.id;
