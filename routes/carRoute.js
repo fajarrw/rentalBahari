30bdae851fe14a74009ce120aa3251a8607602b4
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-11-12 18:24:46 +0700

    fix: change car getById route
    This is done because there was an error when fetching /api/car/search
    and /api/car/searchAvailableCar. This is because /api/car/:id has been
    implemented to fetch car by id, thus "search" and "searchAvailableCar"
    is perceived to be :id. Changing /api/car/:id to /api/car/id/:id solves
    this issue

--- routes/carRoute.js
+++ routes/carRoute.js
@@ -5,7 +5,7 @@ const carController = require("../controllers/carController");
 const authToken = require("../middleware/authToken");
 
 router.get("/all", carController.getAllCar);
-router.get("/:id", carController.getCarById);
+router.get("/id/:id", carController.getCarById);
 router.post("/create", authToken, carController.createCar);
 router.delete("/delete", carController.deleteCar);
 router.put("/edit", carController.editCar);
