2d5ddf2ad716d2ae7a6257a310603a28c29e01be
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2023-11-17 07:20:55 +0700

    feat: create a new rent based on cookie and params

--- routes/rentRoute.js
+++ routes/rentRoute.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const router = express.Router();
 const rentController = require("../controllers/rentController");
+const authenticateToken2 = require("../middleware/authToken2");
 
 router.get("/all", rentController.getAllRent);
-router.post("/create", rentController.createRent);
+router.post("/create", authenticateToken2, rentController.createRent);
 router.delete("/delete", rentController.deleteRent);
 router.put("/edit", rentController.editRent);
 router.get("/search", rentController.searchRent);
