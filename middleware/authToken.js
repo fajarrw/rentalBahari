4ae65373761f37530f178e006afd0064d6e1c1e5
Fajar Rizky Widodo <frizkywidodo@gmail.com>
2025-04-26 13:05:37 +0700

    fix: use authorization header instead of cookie

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = async (req, res, next) => {
-    const token = req.cookies?.token; // Read token from cookie
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Format: "Bearer <token>"
 
     if (!token) return res.sendStatus(401);
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
         if (err) return res.sendStatus(403);
         if (userData.role !== 'admin') return res.sendStatus(401);
--- middleware/authToken2.js
+++ middleware/authToken2.js
@@ -1,9 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken2 = async (req, res, next) => {
-    const token = req.cookies?.token; // Read token from cookie
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Expecting format: "Bearer <token>"
 
     if (!token) return res.sendStatus(401);
+
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
         if (err) return res.sendStatus(403);
         if (userData.role !== 'user' && userData.role !== 'admin') return res.sendStatus(401);
